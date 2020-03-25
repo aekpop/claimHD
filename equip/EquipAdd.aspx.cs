7c2a9993bc8f8c3bb837f5acf63f33a3b717b618
heavenluck <45112461+heavenluck@users.noreply.github.com>
2020-03-25 11:18:20 +0700

    test

--- equip/EquipAdd.aspx.cs
+++ equip/EquipAdd.aspx.cs
@@ -13,7 +13,7 @@ using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.Drawing;
 using System.IO;
-
+//test
 namespace ClaimProject.equip
 {
     public partial class EquipAdd : System.Web.UI.Page
